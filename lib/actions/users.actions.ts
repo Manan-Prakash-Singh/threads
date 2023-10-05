05d00a33cc4d82113bb20181c61f55ba2444f06a
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-10-05 20:32:38 +0530

    added profile card

--- lib/actions/users.actions.ts
+++ lib/actions/users.actions.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import  User from "../models/user.model";
+import Thread from "../models/thread.model";
 import { revalidatePath } from "next/cache";
 import { connectToDB } from "../mongoose"
 
@@ -59,3 +60,36 @@ interface Params {
       throw new Error(`failed to fetch user: ${error.message}`)
     }
   }
+
+
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB();
+
+    // Find all threads authored by the user with the given userId
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: [
+        /*{
+          path: "community",
+          model: Community,
+          select: "name id image _id", // Select the "name" and "_id" fields from the "Community" model
+        }, */
+        {
+          path: "children",
+          model: Thread,
+          populate: {
+            path: "author",
+            model: User,
+            select: "name image id", // Select the "name" and "_id" fields from the "User" model
+          },
+        },
+      ],
+    });
+    return threads;
+  } catch (error) {
+    console.error("Error fetching user threads:", error);
+    throw error;
+  }
+}
